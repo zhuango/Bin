c693d4ece50a65c9ee7f3cebe86bd40fdd98e64f
Og192 <og192liu@gmail.com>
2015-10-22 11:35:13 +0800

    添加系统重建脚本注释

--- SystemRebuild.sh
+++ SystemRebuild.sh
@@ -1,6 +1,9 @@
+#install develop tools.
 sudo apt-get install vim
 sudo apt-get install g++
 sudo apt-get install monodevelop
+
+#install git and download codes.
 sudo apt-get install git
 git clone https://github.com/Og192/CPro
 git clone https://github.com/Og192/CSharp
@@ -11,5 +14,9 @@ git clone https://github.com/Og192/Python
 git clone https://github.com/Og192/Perl
 git clone https://github.com/Og192/Shell_script
 
+#to run TopCoder client.
 sudo apt-get install sudo apt-get install openjdk-7-jdk
+
+#right click to open terminal in a specified folder.
 sudo apt-get install nautilus-open-terminal
+
