88ca5986b2c31913394664e994f57da34be9d595
Og192 <og192liu@gmail.com>
2015-10-22 11:09:34 +0800

    重新安装系统后，安装一些必要软件和下载代码的脚本

---
+++ SystemRebuild.sh
@@ -0,0 +1,15 @@
+sudo apt-get install vim
+sudo apt-get install g++
+sudo apt-get install monodevelop
+sudo apt-get install git
+git clone https://github.com/Og192/CPro
+git clone https://github.com/Og192/CSharp
+git clone https://github.com/Og192/Cpp
+git clone https://github.com/Og192/DesignPatttern
+git clone https://github.com/Og192/Bin
+git clone https://github.com/Og192/Python
+git clone https://github.com/Og192/Perl
+git clone https://github.com/Og192/Shell_script
+
+sudo apt-get install sudo apt-get install openjdk-7-jdk
+sudo apt-get install nautilus-open-terminal
